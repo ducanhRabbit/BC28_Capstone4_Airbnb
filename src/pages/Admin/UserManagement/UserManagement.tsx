85daf7b1870f6a7183eb540b096fa952902b2385
Anh Le <renyk97@gmail.com>
2022-11-04 10:24:39 +0700

    TableData

---
+++ src/pages/Admin/UserManagement/UserManagement.tsx
@@ -0,0 +1,15 @@
+
+import { Typography } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid';
+
+import React from 'react'
+
+type Props = {}
+
+export default function UserManagement({}: Props) {
+  return (
+    <div>
+        <Typography component={'h1'}>Quản lý người dùng</Typography>
+    </div>
+  )
+}
